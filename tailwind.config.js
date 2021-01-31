215db7d077bd0f867d2ed63bf1c81e93b8d4a52d
Adam Gruber <talknmime@gmail.com>
2021-01-31 16:10:50 -0500

    Add postcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: ['./src/*.html'],
+  purge: {
+    content: ['./src/*.html'],
+    options: {
+      keyframes: true,
+    },
+  },
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     fontFamily: {
