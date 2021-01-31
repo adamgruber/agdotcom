04c80f56d73e585827f5796ae9ca58c45f5949a9
Adam Gruber <talknmime@gmail.com>
2021-01-31 15:47:20 -0500

    Add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  purge: ['./src/*.html'],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: 'Work Sans, Helvetica, sans-serif',
+    },
+  },
+};
